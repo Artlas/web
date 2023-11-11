7c493d2a9a78396e7506bdc6a939784ff7159b32
Arthur Fournier <arthur.g.fournier@gmail.com>
2023-11-11 21:38:01 +0100

    add reloading to server

---
+++ reload_request.py
@@ -0,0 +1,18 @@
+from google.auth.transport.requests import Request
+from google.oauth2 import id_token
+import os
+import requests
+import sys
+open_id_connect_token = id_token.fetch_id_token(Request(), os.environ["IAP_CLIENT_ID"])
+headers = {
+    'Authorization': f'Bearer {open_id_connect_token}',
+    'accept': 'application/json',
+    'Content-Type': 'application/json'
+}
+json = {
+    'name': sys.argv[1],
+    'image': 'ghcr.io/artlas/web',
+    'tag': sys.argv[2]
+} 
+x = requests.post("https://docker.fournierfamily.ovh/to_reload",headers=headers, json=json)
+print(x.content.decode("utf-8"))
