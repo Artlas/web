fa18e8f5867c5d4bbdf6ebaadc80e21b2e895104
anna.va <72090408+Jiuyhoi@users.noreply.github.com>
2023-11-16 17:41:17 +0100

    feat: navigation menu in profile

--- src/components/profile_des.tsx
+++ src/components/profile_des.tsx
@@ -7,14 +7,34 @@ interface ProfileProps {
     description: string;
 }
 
-export default function Description({photoProfile, userName, description}: ProfileProps){
+interface ListeProps{
+    listeName: string;
+    picture: string;
+}
+
+export function Description({photoProfile, userName, description}: ProfileProps){
     return(
-        <div className="bg-white dark:bg-gray-800 rounded-lg shadow-lg dark:shadow-none p-4 m-5 items-center">
-            <div className="w-16 h-16 relative">
+        <div className="bg-white dark:bg-gray-800 rounded-lg shadow-lg dark:shadow-none p-4 m-5 flex flex-col items-center">
+            <div className="w-28 h-28 relative">
                 <Image src={photoProfile} alt={userName} layout="fill" objectFit="cover" className="rounded-full" />
             </div>
-            <p>{userName}</p>
-            <p>{description}</p>
+            <span className="text-xl font-semibold m-2">{userName}</span>
+            <span>{description}</span>
         </div>
     );
 }
+
+export function Liste({listeName, picture}:ListeProps){
+    return(
+        <div className="bg-white dark:bg-gray-800 rounded-lg shadow-lg darl:shadow-none p-4 m-5 flex flex-row">
+            <div className="w-24 w24 relative">
+                <Image src={picture} alt={listeName} layout="fill" objectFit="cover" className="rounded-lg"/>
+            </div>
+            <div className="p-2">
+                <h1>{listeName}</h1>
+            </div>
+        </div>
+    );
+}
+
+
--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -20,28 +20,94 @@ export default Profile;
 */
 
 import React from "react"
-import Image from "next/image";
-import Description from "../components/profile_des"
+import {useState } from "react"; 
+import {Description} from "../components/profile_des"
+import {Liste} from "../components/profile_des"
+import Post from "../components/post";
 
 export default function Profile(){
+    const [clickedPost, setclickedPost] = useState(true);
+    const [clickedLiked, setclickedLiked] = useState(false);
+    const [clickedListe, setclickedListe] = useState(false);
+    const [clickedTier, setclickedTier] = useState(false);
+    const handleItemClickPost = () => {
+        setclickedPost(true);
+        setclickedLiked(false);
+        setclickedListe(false);
+        setclickedTier(false);
+
+    };
+    const handleItemClickLiked = () => {
+        setclickedPost(false);
+        setclickedLiked(true);
+        setclickedListe(false);
+        setclickedTier(false);
+    };
+    const handleItemClickListe = () => {
+        setclickedPost(false);
+        setclickedLiked(false);
+        setclickedListe(true);
+        setclickedTier(false);
+    };
+    const handleItemClickTier = () => {
+        setclickedPost(false);
+        setclickedLiked(false);
+        setclickedListe(false);
+        setclickedTier(true);
+    };
     return (
-        <div className="h-full w-full">
-            <div className="inline-flex flex-row">
-                <div className = "object-left-top">
+            <div className="h-full w-full">
+                <div className="max-w-[350px] mx-auto">
                     <hr className="w-auto"></hr>
-                    <div className="inline-flex flex-row text-xl">
-                        <div className=" p-3 m-2 ">Posts</div>
-                        <div className=" p-3 m-2 ">Evénements</div>
-                        <div className=" p-3 m-2 ">J'aime</div>
-                        <div className=" p-3 m-2 ">Listes</div>
-                        <div className=" p-3 m-2 ">Tier Listes</div>
-                    </div>   
+                    <div className="inline-flex flex-row text-xl content-right">
+                        <button className= {`p-1 rounded-lg ${clickedPost ? "bg-black dark:bg-white text-white dark:text-black " : "text-black dark:text-white hover:bg-gray-800"}`} onClick={handleItemClickPost} type="button"> 
+                            <span className="m-3">Posts</span>
+                        </button>
+                        <button className={`p-1 rounded-lg ${clickedLiked ? "bg-black dark:bg-white text-white dark:text-black " : "text-black dark:text-white hover:bg-gray-800"}`} onClick={handleItemClickLiked} type="button"> 
+                            <span className="m-2">J'aime</span>
+                        </button>
+                        <button className={`p-1 rounded-lg ${clickedListe ? "bg-black dark:bg-white text-white dark:text-black " : "text-black dark:text-white hover:bg-gray-800"}`} onClick={handleItemClickListe} type="button"> 
+                            <span className="m-2">Listes</span>
+                        </button>
+                        <button className={`p-1 rounded-lg ${clickedTier ? "bg-black dark:bg-white text-white dark:text-black " : "text-black dark:text-white hover:bg-gray-800"}`} onClick={handleItemClickTier} type="button"> 
+                            <span className="m-2">Tier Listes</span>
+                        </button>
+                    </div>
                 </div>
-                <div className="object-right-top ">
-                    <Description photoProfile="/pp-image-ex.jpg" userName="Michelle" description="Bonjour, je suis une artiste" />
+                <main className="flex w-full">
+                <div className="h-full w-full flex flex-row">
+                    
+                    {clickedPost ?
+                        <div className = "max-w-[800px] mx-auto justify-right grow">
+                            <Post title="Star Wars" description="Star Wars Prequels" mediaUrl="/Star-Wars-Prequels.jpg" />
+                        </div> 
+                        : null
+                    }
+                    {clickedLiked ?
+                        <div className = "max-w-[800px] justify-center mx-auto grow">
+                            <Post title="Super Mario Film" description="Mario has to save Peach" mediaUrl="/mario-film.jpg" />
+                        </div> 
+                        : null
+                    }
+                    {clickedListe ?
+                        <div className = "max-w-[800px] justify-center mx-auto grow">
+                            <Liste listeName="cinéma" picture="/pp-image-ex.jpg"/>
+                        </div> 
+                        : null
+                    }
+                    {clickedTier ?
+                        <div className = "max-w-[800px] justify-center mx-auto grow">
+                            <Liste listeName="Tier Liste Jeux vidéo" picture="/pp-image-ex.jpg"/>
+                        </div> 
+                        : null
+                    }
+                    <div className="justify-right min-w-[400px]">
+                        <Description photoProfile="/pp-image-ex.jpg" userName="Michelle" description="Bonjour, je suis une artiste" />
+                    </div>  
                 </div>
+
+                </main>
             </div>
-        </div>
 
     );
 }
