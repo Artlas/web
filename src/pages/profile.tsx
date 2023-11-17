4d6df4c7cbb3f2e9b841dc1906fe18d7938c8694
anna.va <72090408+Jiuyhoi@users.noreply.github.com>
2023-11-17 17:26:00 +0100

    feat: add display friends of user

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -20,10 +20,11 @@ export default Profile;
 */
 
 import React from "react"
-import {useState } from "react"; 
+import {useState } from "react";
 import {Description} from "../components/profile_des"
 import {Liste} from "../components/profile_des"
 import Post from "../components/post";
+import {Friend} from "../components/profile_des"
 
 export default function Profile(){
     const [clickedPost, setclickedPost] = useState(true);
@@ -56,58 +57,63 @@ export default function Profile(){
         setclickedTier(true);
     };
     return (
-            <div className="h-full w-full">
-                <div className="max-w-[350px] mx-auto">
-                    <hr className="w-auto"></hr>
-                    <div className="inline-flex flex-row text-xl content-right">
-                        <button className= {`p-1 rounded-lg ${clickedPost ? "bg-black dark:bg-white text-white dark:text-black " : "text-black dark:text-white hover:bg-gray-800"}`} onClick={handleItemClickPost} type="button"> 
-                            <span className="m-3">Posts</span>
-                        </button>
-                        <button className={`p-1 rounded-lg ${clickedLiked ? "bg-black dark:bg-white text-white dark:text-black " : "text-black dark:text-white hover:bg-gray-800"}`} onClick={handleItemClickLiked} type="button"> 
-                            <span className="m-2">J'aime</span>
-                        </button>
-                        <button className={`p-1 rounded-lg ${clickedListe ? "bg-black dark:bg-white text-white dark:text-black " : "text-black dark:text-white hover:bg-gray-800"}`} onClick={handleItemClickListe} type="button"> 
-                            <span className="m-2">Listes</span>
-                        </button>
-                        <button className={`p-1 rounded-lg ${clickedTier ? "bg-black dark:bg-white text-white dark:text-black " : "text-black dark:text-white hover:bg-gray-800"}`} onClick={handleItemClickTier} type="button"> 
-                            <span className="m-2">Tier Listes</span>
-                        </button>
-                    </div>
+        <div className="h-full w-full">
+            <div className="max-w-[350px] mx-auto">
+                <hr className="w-auto"></hr>
+                <div className="inline-flex flex-row text-xl content-left">
+                    <button className= {`p-1 rounded-lg ${clickedPost ? "bg-black dark:bg-white text-white dark:text-black " : "text-black dark:text-white hover:bg-gray-800"}`} onClick={handleItemClickPost} type="button"> 
+                        <span className="m-3">Posts</span>
+                    </button>
+                    <button className={`p-1 rounded-lg ${clickedLiked ? "bg-black dark:bg-white text-white dark:text-black " : "text-black dark:text-white hover:bg-gray-800"}`} onClick={handleItemClickLiked} type="button"> 
+                        <span className="m-2">J'aime</span>
+                    </button>
+                    <button className={`p-1 rounded-lg ${clickedListe ? "bg-black dark:bg-white text-white dark:text-black " : "text-black dark:text-white hover:bg-gray-800"}`} onClick={handleItemClickListe} type="button"> 
+                        <span className="m-2">Listes</span>
+                    </button>
+                    <button className={`p-1 rounded-lg ${clickedTier ? "bg-black dark:bg-white text-white dark:text-black " : "text-black dark:text-white hover:bg-gray-800"}`} onClick={handleItemClickTier} type="button"> 
+                        <span className="m-2">Tier Listes</span>
+                    </button>
                 </div>
-                <main className="flex w-full">
+            </div>
+            <main className="flex w-full">
                 <div className="h-full w-full flex flex-row">
-                    
                     {clickedPost ?
                         <div className = "max-w-[800px] mx-auto justify-right grow">
                             <Post title="Star Wars" description="Star Wars Prequels" mediaUrl="/Star-Wars-Prequels.jpg" />
-                        </div> 
+                        </div>
                         : null
                     }
                     {clickedLiked ?
                         <div className = "max-w-[800px] justify-center mx-auto grow">
                             <Post title="Super Mario Film" description="Mario has to save Peach" mediaUrl="/mario-film.jpg" />
-                        </div> 
+                        </div>
                         : null
                     }
                     {clickedListe ?
                         <div className = "max-w-[800px] justify-center mx-auto grow">
                             <Liste listeName="cinéma" picture="/pp-image-ex.jpg"/>
-                        </div> 
+                        </div>
                         : null
                     }
                     {clickedTier ?
                         <div className = "max-w-[800px] justify-center mx-auto grow">
                             <Liste listeName="Tier Liste Jeux vidéo" picture="/pp-image-ex.jpg"/>
-                        </div> 
+                        </div>
                         : null
                     }
-                    <div className="justify-right min-w-[400px]">
-                        <Description photoProfile="/pp-image-ex.jpg" userName="Michelle" description="Bonjour, je suis une artiste" />
-                    </div>  
+                    <div className="justify-right min-w-[400px] flex flex-col">
+                        <Description photoProfile="/pp-image-ex.jpg" userName="Michelle" description="Bonjour, je suis une artiste" preference="Musique" loisir="Sport" birthday="10/11/2001" account_birthday="17/11/2023" address="Paris"/>
+                        <span className="pl-5 text-2xl font-semibold">Amis</span>
+                        <div className="flex flex-row bg-white dark:bg-gray-800 rounded-lg shadow-lg darl:shadow-none m-5">
+                            <Friend photoProfile="/pp-image-ex.jpg" userName="Anna"/>
+                            <Friend photoProfile="/pp-image-ex.jpg" userName="Anna"/>
+                            <Friend photoProfile="/pp-image-ex.jpg" userName="Anna"/>
+                        </div>
+                    </div>
                 </div>
 
-                </main>
-            </div>
+            </main>
+        </div>
 
     );
 }
