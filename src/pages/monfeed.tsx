d085d938d832249cf814797f31d28ab771189376
Ahddry <78737719+Ahddry@users.noreply.github.com>
2024-01-02 16:31:21 +0100

    feat: post component front-end
    Added the Post component: if given the information of a post as parameters, will display a social media post with everything.
    
    TODO:
    Backend: almost everything
    Frontend: open images in fullscreen, share button, and display information specific to certain categories.

--- src/pages/monfeed.tsx
+++ src/pages/monfeed.tsx
@@ -2,12 +2,63 @@ import Image from "next/image";
 import Post from "../components/post";
 
 export default function Monfeed() {
+    const tempPost1 = {
+        id: 1,
+        title: "C'est très joli",
+        description: "J'aime vraiment beaucoup trop ces photos, elles sont absolument magnifiques, je suis fan",
+        category: "Photographie",
+        subCategory: "Photos",
+        illustration: ["https://picsum.photos/450", "https://picsum.photos/1455", "https://picsum.photos/464/700", "https://picsum.photos/1450/700"],
+        postDate: new Date(),
+        releaseDate: new Date(),
+        isMediaTypeImages: true,
+    };
+
+    const tempPost2 = {
+        id: 1,
+        title: "Star Wars",
+        description: "Star Wars 3 trailer",
+        category: "Cinema",
+        subCategory: "Films",
+        video: "https://youtu.be/t1qtvKYwTV0",
+        postDate: new Date(),
+        releaseDate: new Date(),
+        isMediaTypeImages: true,
+    };
+
+    const tempPost3 = {
+        id: 1,
+        title: "Twitch.tv",
+        description: "Une video de twitch",
+        category: "Cinema",
+        subCategory: "Films",
+        video: "https://www.twitch.tv/videos/1778232094?collection=sHU0nUioUBcc0Q",
+        postDate: new Date(),
+        releaseDate: new Date(),
+        isMediaTypeImages: true,
+    };
+
+    const tempPost4 = {
+        id: 1,
+        title: "Dailymotion",
+        description: "Une video de daily motion à la verticale",
+        category: "Cinema",
+        subCategory: "Films",
+        video: "https://www.dailymotion.com/video/x8pvuxl",
+        postDate: new Date(),
+        releaseDate: new Date(),
+        isMediaTypeImages: true,
+    };
+
     return (
         <main className="flex w-full">
             <div className="h-full w-full">
                 <h1 className="text-4xl font-bold dark:text-white">Bienvenue sur votre feed</h1>
                 <div className="max-w-[800px] justify-center mx-auto">
-                    <Post title="Star Wars" description="Star Wars Prequels" mediaUrl="/Star-Wars-Prequels.jpg" />
+                    <Post {...tempPost1} />
+                    <Post {...tempPost2} />
+                    <Post {...tempPost3} />
+                    <Post {...tempPost4} />
                 </div>
             </div>
         </main>
